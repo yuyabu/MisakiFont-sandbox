3e736d60f876e971b86ca0282da67ed49cbed416
yuyabu <yuyabushita@gmail.com>
2018-08-09 01:09:41 +0900

    add file read sample

--- font.c
+++ font.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "font.h"
 
 int main(){
+  FILE *fp;
+  char *filename = "MISAKI.FNT";
 
+  /*fnt file open*/
+  if((fp = fopen(filename,"r"))==NULL){
+    fprintf(stderr,"%s open faild.\n",filename);
+    exit(EXIT_FAILURE);
+  }
+  char c;
+  for(int i=0;i<10;i++){
+    c = fgetc(fp);
+    printf("%c\n",c);
+  }
+  fclose(fp);
 }
