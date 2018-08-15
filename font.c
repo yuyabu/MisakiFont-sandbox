8e978764ce8dd43bc003f78c263424f708ae2157
yuyabu <yuyabushita@gmail.com>
2018-08-15 16:58:26 +0900

    とりあえずコミット。もっとコミット粒度を落とさねば

--- font.c
+++ font.c
@@ -4,7 +4,11 @@
 
 #include "font.h"
 
+typedef enum _boolean { FALSE, TRUE } boolean;
 
+void confirm_files_sandbox(fontx2header header,FILE *fp);
+void toggle_endian(char v[2]);
+boolean comp_letter(char v1[2],char v2[2]);
 int main(){
   FILE *fp;
   char *filename = "MISAKI.FNT";
@@ -16,79 +20,104 @@ int main(){
   };
  fontx2header header;
  printf("hdader size is %lu\n",sizeof(header));
- fread(&header,sizeof(header),1,fp);
- // typedef struct{
- //   char filesignature[6];
- //   char fontname[8];
- //   char font_w;
- //   char font_h;
- //   char char_code_flg;
- //   char blocks;
- //   char start_code[2];
- //   char end_code[2];
- // } fontx2header;
+ fread(&header,HEADER_SIZE,1,fp);
 
- //この書き方はできないみたい。filesignatureの開始アドレスからnull文字までが全部出てしまう。
- //printf("filesignature is %s",header.filesignature);
+ //動作確認用。
+ //confirm_files_sandbox(header,fp);
 
- char temp[100];
- strncpy(temp,header.filesignature,6);
+ //bloack_table_entry entry[block_amount];
+ //header.block = malloc(sizeof(bloack_table_entry) * block_amount);
+ printf("hdader size is %lu\n",sizeof(header));
+ printf("table entry pointer size is %2lu\n",sizeof(bloack_table_entry*));
+ printf("table entry  size is        %2lu\n",sizeof(bloack_table_entry));
 
- printf("filesignature is %s\n" ,temp                );
+ printf("new hader size is           %2lu\n",sizeof(fontx2header));
+ printf("old header size is          %2lu\n",sizeof(fontx2header_before));
 
- strncpy(temp,header.fontname,7);
+ printf("size of hader instance      %2lu\n",sizeof(header.block));
 
- printf("fontname is %s\n"      ,temp                );
- printf("width is %x\n"         ,header.font_w       );
- printf("hight is %x\n"         ,header.font_h       );
- printf("char code flg is %x\n" ,header.char_code_flg);
- printf("blocks is %x\n"        ,header.blocks       );
+ printf("addr of header          %p\n",&header);
+ printf("addr of header.block    %p\n",&header.block);
+ printf("addr of header.block[0] %p\n",&header.block[1]);
+ printf("addr of header.block[1] %p\n",&header.block[1]);
 
- //char型のblock数をint型に変換
- //charとintでバイト数違うけどこんな型変換ができていいのかw
+
+
+
  int block_amount = (int)header.blocks;
+ header.block = malloc(sizeof(bloack_table_entry) * block_amount);
 
- bloack_table_entry entry[block_amount];
-
- //ブロックテーブルの読み取り
- fread(&entry,sizeof(entry),1,fp);
+ fread(header.block,sizeof(bloack_table_entry)* block_amount,1,fp);
  for(int i=0;i<block_amount;i++){
-   //この方法でなぜかダンプできない
-   printf("%3d blocks start code is 0x%2x%2x\n",i
-     ,(unsigned char)entry[i].start_code[0]
-     ,(unsigned char)entry[i].start_code[1]
+   toggle_endian(header.block[i].start_code);
+   toggle_endian(header.block[i].end_code);
 
+
+   printf("%3d blocks start code is 0x%2x%2x\n",i
+     ,(unsigned char)header.block[i].start_code[0]
+     ,(unsigned char)header.block[i].start_code[1]
    );
    printf("%3d blocks end   code is 0x%2x%2x\n",i
-   ,(unsigned char)entry[i].end_code[0]
-   ,(unsigned char)entry[i].end_code[1]
-  );
-}
-
-font_data data[10];
-fread(&data,sizeof(data),1,fp);
-
- for(int i=0;i<10;i++){
-   print_font(data[i]);
- }
-
-
-
- //配列をそのまま１６進数で表示する方法がわからないのでかなり頭の悪いやり方でやってる
- //printf("start_code is %x%x\n"   ,(unsigned char)header.start_code[0] ,(unsigned char)header.start_code[1]);
- //printf("end_code is %02x%02x\n" ,(unsigned char)header.end_code[0]   ,(unsigned char)header.end_code[1]  );
+    ,(unsigned char)header.block[i].end_code[0]
+    ,(unsigned char)header.block[i].end_code[1]
+    );
+  }
 
   fclose(fp);
+
+
+
 }
+
+/**
+ * 8バイトの配列(美咲フォントのデータ実態)を１文字分printする
+ * @param c [description]
+ */
 void print_font(font_data data){
   for(int i=0;i<8;i++){
     printbincharpad(data[i]);
   }
   puts("\n");
 }
-void printbincharpad(char c){
+
+/**
+ * 美咲フォント
+ * @param c [description]
+ */
+ void printbincharpad(char c){
   for (int i = 7; i >= 0; --i){
     putchar( (c & (1 << i)) ? '1' : '0' );
   }
   putchar('\n');
+}
+int block_potition(){
+  return -1;
+}
+
+/**
+ * 長さ2の配列のエンディアンを入れ替える
+ * @param v      [処理対象の配列]
+ * @param length [配列の長さ]
+ */
+void toggle_endian(char v[2]){
+  char tmp=v[0];
+  v[0]= v[1];
+  v[1] = tmp;
+}
+
+/**
+ * 文字(2Byete=長さ2のchar配列の比較)
+ * @param  v1 比較対象1
+ * @param  v2 比較対象2
+ * @return v1>v2のときtrue。それ以外false
+ */
+boolean comp_letter(char v1[2],char v2[2]){
+  if(v1[0]>v2[1]){
+    return TRUE;
+  }else if(v1[0]==v2[0]){
+    if(v1[1]>v2[1]){
+      return TRUE;
+    }
+  }
+  return FALSE;
 }
--- font.h
+++ font.h
@@ -1,3 +1,10 @@
+#define tablesize 92
+#define HEADER_SIZE 18
+
+typedef struct{
+  char start_code[2];
+  char end_code[2];
+}bloack_table_entry;
 
 typedef struct{
   char filesignature[6];
@@ -6,12 +13,20 @@ typedef struct{
   char font_h;
   char char_code_flg;
   char blocks;
-} fontx2header;
+  bloack_table_entry *block;
+}fontx2header;
 
 typedef struct{
-  char start_code[2];
-  char end_code[2];
-}bloack_table_entry;
+  char filesignature[6];
+  char fontname[8];
+  char font_w;
+  char font_h;
+  char char_code_flg;
+  char blocks;
+  //  bloack_table_entry *block;
+}fontx2header_before;
+
+typedef bloack_table_entry test_table[];
 
 typedef char font_data[8];
 
